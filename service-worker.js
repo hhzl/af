78de81d799a333370393cc9649ce127f912741dc
hhzl <hannes.hirzel@gmail.com>
2017-01-05 14:23:35 +0100

    version 18b

--- service-worker.js
+++ service-worker.js
@@ -14,20 +14,16 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v18a';
+const PRECACHE = 'precache-v18b';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
-  'BasicServiceWorkerSample.html',
   './', // Alias for index.html
   'styles.css',
-  'main.css',
-  'demo.js',
   'LW.js',
-  'wordlist-en-ge.js',
-  'papaparse.js'
+  'wordlist'
 ];
 
 // The install handler takes care of precaching the resources we always need.
