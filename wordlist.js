1800ddc4969e96a255236e01930859eb3ced0e04
hhzl <hannes.hirzel@gmail.com>
2017-01-05 10:37:26 +0100

    Trimmed copy of LearnWords2-SWapp

---
+++ wordlist.js
@@ -0,0 +1,542 @@
+var wordList = [
+  {
+    "_id": 1,
+    "word": "apple",
+    "translate": "der Apfel",
+    "domain": "fruits"
+  },
+  {
+    "_id": 2,
+    "word": "orange",
+    "translate": "die Orange",
+    "domain": "fruits"
+  },
+  {
+    "_id": 3,
+    "word": "pawpaw",
+    "translate": "die Papaya",
+    "domain": "fruits"
+  },
+  {
+    "_id": 4,
+    "word": "raspberry",
+    "translate": "die Himbeere",
+    "domain": "fruits"
+  },
+  {
+    "_id": 5,
+    "word": "strawberry",
+    "translate": "die Erdbeere",
+    "domain": "fruits"
+  },
+  {
+    "_id": 6,
+    "word": "apricot",
+    "translate": "die Aprikose",
+    "domain": "fruits"
+  },
+  {
+    "_id": 7,
+    "word": "coconut",
+    "translate": "die Kokosnuss",
+    "domain": "fruits"
+  },
+  {
+    "_id": 8,
+    "word": "pear",
+    "translate": "die Birne",
+    "domain": "fruits"
+  },
+  {
+    "_id": 9,
+    "word": "pineapple",
+    "translate": "die Ananas",
+    "domain": "fruits"
+  },
+  {
+    "_id": 10,
+    "word": "guava",
+    "translate": "die Guava",
+    "domain": "fruits"
+  },
+  {
+    "_id": 11,
+    "word": "banana",
+    "translate": "die Banane",
+    "domain": "fruits"
+  },
+  {
+    "_id": 12,
+    "word": "melon",
+    "translate": "die Melone",
+    "domain": "fruits"
+  },
+  {
+    "_id": 13,
+    "word": "lemon",
+    "translate": "die Zitrone",
+    "domain": "fruits"
+  },
+  {
+    "_id": 14,
+    "word": "plum",
+    "translate": "die Pflaume",
+    "domain": "fruits"
+  },
+  {
+    "_id": 15,
+    "word": "grape",
+    "translate": "die Traube",
+    "domain": "fruits"
+  },
+  {
+    "_id": 16,
+    "word": "mango",
+    "translate": "die Mango",
+    "domain": "fruits"
+  },
+  {
+    "_id": 17,
+    "word": "lime",
+    "translate": "die Limette",
+    "domain": "fruits"
+  },
+  {
+    "_id": 18,
+    "word": "cherry",
+    "translate": "die Kirsche",
+    "domain": "fruits"
+  },
+  {
+    "_id": 19,
+    "word": "kiwi",
+    "translate": "die Kiwi",
+    "domain": "fruits"
+  },
+  {
+    "_id": 20,
+    "word": "blueberry",
+    "translate": "die Heidelbeere",
+    "domain": "fruits"
+  },
+  {
+    "_id": 21,
+    "word": "blackberry",
+    "translate": "die Brombeere",
+    "domain": "fruits"
+  },
+  {
+    "_id": 22,
+    "word": "onion",
+    "translate": "die Zwiebel",
+    "domain": "fruits"
+  },
+  {
+    "_id": 23,
+    "word": "red currant",
+    "translate": "die Johannisbeere",
+    "domain": "fruits"
+  },
+  {
+    "_id": 24,
+    "word": "black currant",
+    "translate": "die schwarze Johannisbeere",
+    "domain": "fruits"
+  },
+  {
+    "_id": 25,
+    "word": "potato",
+    "translate": "die Kartoffel",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 26,
+    "word": "okro",
+    "translate": "die Okra",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 27,
+    "word": "mushroom",
+    "translate": "der Pilz",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 28,
+    "word": "tomato",
+    "translate": "die Tomate",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 29,
+    "word": "garden egg",
+    "translate": "die Aubergine",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 30,
+    "word": "bean",
+    "translate": "die Bohne",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 31,
+    "word": "plantain",
+    "translate": "die Kochbanane",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 32,
+    "word": "soy bean",
+    "translate": "die Sojabohne",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 33,
+    "word": "cocoyam",
+    "translate": "der Taro",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 34,
+    "word": "cassava",
+    "translate": "der Maniok",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 35,
+    "word": "melon seeds",
+    "translate": "die Melonensamen",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 36,
+    "word": "yam",
+    "translate": "die Yamswurzel",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 37,
+    "word": "green pepper",
+    "translate": "der grüne Paprika",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 38,
+    "word": "carrot",
+    "translate": "die Karotte",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 39,
+    "word": "cabbage",
+    "translate": "der Kohl",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 40,
+    "word": "broccoli",
+    "translate": "der Broccoli",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 41,
+    "word": "cauliflower",
+    "translate": "der Blumenkohl",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 42,
+    "word": "spinach",
+    "translate": "der Spinat",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 43,
+    "word": "pea",
+    "translate": "die Erbse",
+    "domain": "vegetables"
+  },
+  {
+    "_id": 44,
+    "word": "sausage",
+    "translate": "die Wurst",
+    "domain": "food"
+  },
+  {
+    "_id": 45,
+    "word": "jam",
+    "translate": "die Marmelade / Konfitüre",
+    "domain": "food"
+  },
+  {
+    "_id": 46,
+    "word": "ham",
+    "translate": "der Schinken",
+    "domain": "food"
+  },
+  {
+    "_id": 47,
+    "word": "garlic",
+    "translate": "der Knoblauch",
+    "domain": "spices"
+  },
+  {
+    "_id": 48,
+    "word": "vinegar",
+    "translate": "der Essig",
+    "domain": "spices"
+  },
+  {
+    "_id": 49,
+    "word": "ketchup",
+    "translate": "der Ketchup",
+    "domain": "spices"
+  },
+  {
+    "_id": 50,
+    "word": "water",
+    "translate": "das Wasser",
+    "domain": "food"
+  },
+  {
+    "_id": 51,
+    "word": "salt",
+    "translate": "das Salz",
+    "domain": "spices"
+  },
+  {
+    "_id": 52,
+    "word": "ginger",
+    "translate": "der Ingwer",
+    "domain": "spices"
+  },
+  {
+    "_id": 53,
+    "word": "avocado",
+    "translate": "die Avocdao",
+    "domain": "fruits"
+  },
+  {
+    "_id": 54,
+    "word": "food",
+    "translate": "das Essen",
+    "domain": "food"
+  },
+  {
+    "_id": 55,
+    "word": "to be hungry",
+    "translate": "Hunger haben",
+    "domain": "food"
+  },
+  {
+    "_id": 56,
+    "word": "breakfast",
+    "translate": "das Frühstück",
+    "domain": "food"
+  },
+  {
+    "_id": 57,
+    "word": "lunch",
+    "translate": "das Mittagessen",
+    "domain": "food"
+  },
+  {
+    "_id": 58,
+    "word": "supper",
+    "translate": "das Abendessen",
+    "domain": "food"
+  },
+  {
+    "_id": 59,
+    "word": "kitchen",
+    "translate": "die Küche",
+    "domain": "food"
+  },
+  {
+    "_id": 60,
+    "word": "soup",
+    "translate": "die Suppe",
+    "domain": "food"
+  },
+  {
+    "_id": 61,
+    "word": "bakery",
+    "translate": "die Bäckerei",
+    "domain": "food"
+  },
+  {
+    "_id": 62,
+    "word": "shop",
+    "translate": "das Geschäft",
+    "domain": "food"
+  },
+  {
+    "_id": 63,
+    "word": "supermarket",
+    "translate": "der Supermarkt",
+    "domain": "food"
+  },
+  {
+    "_id": 64,
+    "word": "market",
+    "translate": "der Markt",
+    "domain": "food"
+  },
+  {
+    "_id": 65,
+    "word": "food",
+    "translate": "die Lebensmittel",
+    "domain": "food"
+  },
+  {
+    "_id": 66,
+    "word": "bread",
+    "translate": "das Brot",
+    "domain": "food"
+  },
+  {
+    "_id": 67,
+    "word": "butter",
+    "translate": "die Butter",
+    "domain": "food"
+  },
+  {
+    "_id": 68,
+    "word": "milk",
+    "translate": "die Milch",
+    "domain": "food"
+  },
+  {
+    "_id": 69,
+    "word": "cheese",
+    "translate": "der Käse",
+    "domain": "food"
+  },
+  {
+    "_id": 70,
+    "word": "egg",
+    "translate": "das Ei",
+    "domain": "food"
+  },
+  {
+    "_id": 71,
+    "word": "meat",
+    "translate": "das Fleisch",
+    "domain": "food"
+  },
+  {
+    "_id": 72,
+    "word": "fish",
+    "translate": "der Fisch",
+    "domain": "food"
+  },
+  {
+    "_id": 73,
+    "word": "vegetable",
+    "translate": "das Gemüse",
+    "domain": "food"
+  },
+  {
+    "_id": 74,
+    "word": "salad",
+    "translate": "der Salat",
+    "domain": "food"
+  },
+  {
+    "_id": 75,
+    "word": "oil",
+    "translate": "das Öl",
+    "domain": "food"
+  },
+  {
+    "_id": 76,
+    "word": "cake",
+    "translate": "der Kuchen",
+    "domain": "food"
+  },
+  {
+    "_id": 77,
+    "word": "rice",
+    "translate": "der Reis",
+    "domain": "food"
+  },
+  {
+    "_id": 78,
+    "word": "drink",
+    "translate": "das Getränk",
+    "domain": "food"
+  },
+  {
+    "_id": 79,
+    "word": "coffee",
+    "translate": "der Kaffee",
+    "domain": "food"
+  },
+  {
+    "_id": 80,
+    "word": "tea",
+    "translate": "der Tee",
+    "domain": "food"
+  },
+  {
+    "_id": 81,
+    "word": "juice",
+    "translate": "der Saft",
+    "domain": "food"
+  },
+  {
+    "_id": 82,
+    "word": "flour",
+    "translate": "das Mehl",
+    "domain": "food"
+  },
+  {
+    "_id": 83,
+    "word": "sugar",
+    "translate": "der Zucker",
+    "domain": "food"
+  },
+  {
+    "_id": 84,
+    "word": "buy",
+    "translate": "kaufen",
+    "domain": "actions"
+  },
+  {
+    "_id": 85,
+    "word": "noodles",
+    "translate": "die Nudeln",
+    "domain": "food"
+  },
+  {
+    "_id": 86,
+    "word": "spaghetti",
+    "translate": "die Spaghetti",
+    "domain": "food"
+  },
+  {
+    "_id": 87,
+    "word": "macaroni",
+    "translate": "die Makkaroni",
+    "domain": "food"
+  },
+  {
+    "_id": 88,
+    "word": "chicken",
+    "translate": "das Huhn",
+    "domain": "food"
+  },
+  {
+    "_id": 89,
+    "word": "cook",
+    "translate": "kochen",
+    "domain": "actions"
+  },
+  {
+    "_id": 90,
+    "word": "essen",
+    "translate": "eat",
+    "domain": "actions"
+  }
+];
